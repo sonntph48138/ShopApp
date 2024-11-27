7217fb83bbed6ca77f0b84786f15ee69b77f396f
sonntph48138 <sonntph48138@fpt.edu.vn>
2024-11-27 15:05:05 +0700

    update database

--- shopapp-backend/database.sql
+++ shopapp-backend/database.sql
@@ -13,7 +13,10 @@ CREATE TABLE users(
     is_active TINYINT(1) DEFAULT 1, 
     date_of_birth DATE,
     facebook_account_id INT DEFAULT 0,
-    google_account_id INT DEFAULT 0
+    google_account_id INT DEFAULT 0,
+    role_id int DEFAULT '1',
+    email varchar(255) DEFAULT '',
+    profile_image varchar(255) DEFAULT ''
 );
 -- role
 ALTER TABLE users ADD COLUMN role_id INT;
@@ -30,6 +33,8 @@ CREATE TABLE tokens(
     expiration_date DATETIME,
     revoked TINYINT(1) NOT NULL,
     expire TINYINT(1) NOT NULL,
+    refresh_token varchar(255) DEFAULT '',
+    refresh_expiration_date datetime DEFAULT NULL
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -65,23 +70,26 @@ CREATE TABLE products(
 );
 
 -- Đặt hàng- order
-CREATE TABLE orders(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    fullname VARCHAR(100) DEFAULT'',
-    email VARCHAR(255) DEFAULT'',
-    phone_number VARCHAR(20) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    note VARCHAR(100) DEFAULT'',
-    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20),
-    total_money FLOAT CHECK(total_money >= 0)
+CREATE TABLE orders (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int DEFAULT NULL,
+  fullname varchar(100) DEFAULT '',
+  email varchar(100) DEFAULT '',
+  phone_number varchar(20) NOT NULL,
+  address varchar(200) NOT NULL,
+  note varchar(100) DEFAULT '',
+  order_date datetime DEFAULT CURRENT_TIMESTAMP,
+  status enum('pending','processing','shipped','delivered','cancelled') DEFAULT NULL COMMENT 'Trạng thái đơn hàng',
+  total_money float DEFAULT NULL,
+  active tinyint(1) DEFAULT NULL,
+  coupon_id int DEFAULT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (coupon_id) REFERENCES coupons(id)
 );
 
 ALTER TABLE orders ADD COLUMN shipping_method VARCHAR(100);
 ALTER TABLE orders ADD COLUMN shipping_address VARCHAR(200);
-ALTER TABLE orders ADD COLUMN shipping_datte DATE;
+ALTER TABLE orders ADD COLUMN shipping_date DATE;
 ALTER TABLE orders ADD COLUMN tracking_number VARCHAR(100);
 ALTER TABLE orders ADD COLUMN payment_method VARCHAR(100);
 -- Xóa 1 đơn hàng - Xóa mềm
@@ -98,6 +106,7 @@ CREATE TABLE order_details(
     number_of_products INT CHECK(number_of_products > 0),
     total_money FLOAT CHECK(total_money >= 0),
     color VARCHAR(20) DEFAULT'',
+    coupon_id int DEFAULT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
@@ -111,3 +120,50 @@ CREATE TABLE product_images(
     FOREIGN KEY (product_id) REFERENCES products(id) on DELETE CASCADE
 )
 
+CREATE TABLE comments (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  product_id int DEFAULT NULL,
+  user_id int DEFAULT NULL,
+  content varchar(255) DEFAULT NULL,
+  created_at datetime DEFAULT NULL,
+  updated_at datetime DEFAULT NULL
+) 
+
+
+
+CREATE TABLE coupons (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  code varchar(50) NOT NULL,
+  active tinyint(1) NOT NULL DEFAULT '1'
+)
+
+CREATE TABLE favorites (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int DEFAULT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  product_id int DEFAULT NULL,
+  FOREIGN KEY (product_id) REFERENCES products(id)
+)
+
+CREATE TABLE flyway_schema_history (
+  installed_rank int PRIMARY KEY AUTO_INCREMENT,
+  version varchar(50) DEFAULT NULL,
+  description varchar(200) NOT NULL,
+  type varchar(20) NOT NULL,
+  script varchar(1000) NOT NULL,
+  checksum int DEFAULT NULL,
+  installed_by varchar(100) NOT NULL,
+  installed_on timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  execution_time int NOT NULL,
+  success tinyint(1) NOT NULL
+)
+
+CREATE TABLE coupon_conditions (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  attribute varchar(255) NOT NULL,
+  operator varchar(10) NOT NULL,
+  value varchar(255) NOT NULL,
+  discount_amount decimal(5,2) NOT NULL,
+  coupon_id int NOT NULL,
+  FOREIGN KEY (coupon_id) REFERENCES coupons(id)
+)
